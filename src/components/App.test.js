1e9977409a1c09b3332c96946d0d3e6f9fe6c94e
Maxemillian Wood <Max W>
2018-09-25 18:40:41 -0500

    fixed first test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import App from './app';
+import App from './App';
 
 describe('<App />', () => {
     it('Renders without crashing', () => {
